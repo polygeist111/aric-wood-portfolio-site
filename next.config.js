1f70b191cd8cfbd6c34b95bb94741a0aaf507dad
Thalia Wood <42354895+polygeist111@users.noreply.github.com>
2025-10-24 19:26:16 -0500

    Update next.config.js
    Add assetPrefix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@
 //  experimental: { serverActions: { bodySizeLimit: '2mb' } },
 //};
 const nextConfig = {
-  basePath: "/aric-wood-portfolio-site",
+  basePath: "/aric-wood-portfolio-site", // these two lines likely not necessary once on custom domain
+  assetPrefix: "/aric-wood-portfolio-site/",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
