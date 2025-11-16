fdb15db29c8a4c07c7004aa02dd604a551645be6
Thalia Wood <42354895+polygeist111@users.noreply.github.com>
2025-11-15 19:06:11 -0700

    Cloudflare compatibility update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-/** @type {import('next').NextConfig} */
-// removed portion given by vibecode, replaced with section from github pages tutorial
-//const nextConfig = {
-//  experimental: { serverActions: { bodySizeLimit: '2mb' } },
-//};
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
-  basePath: "/aric-wood-portfolio-site", // these two lines likely not necessary once on custom domain
-  assetPrefix: "/aric-wood-portfolio-site/",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  output: "export",
 };
 
 module.exports = nextConfig;
