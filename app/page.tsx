d71fa31257aab36a15f891cfa7c43dcf91e9e691
Thalia Wood <42354895+polygeist111@users.noreply.github.com>
2025-11-27 18:48:02 -0700

    Image responsiveness changes

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
       <main id="home">
         <section id="hook" className="border-b border-gray-200">
           <div className="container py-16 grid grid-cols-[min-content_1fr] gap-6">
-            <div className="w-full min-w-[200px] max-w-[400px]">
+            <div className="w-full min-w-3xs max-w-[400px]">
               <h1 className="text-5xl text-transforma-red leading-tight tracking-tight m-0">Strategy to Reality</h1>
               <p>My work and ideas across Strategy Activation, Change & Transformation, Vision & Strategic Planning, and Facilitation.</p>
               <div className="flex gap-3 flex-wrap">
@@ -44,7 +44,7 @@ export default function Page() {
                 <a href="#contact" className="btn btn-outline">Contact Me →</a>
               </div>
             </div>
-            <div className="w-full min-w-[600px]">
+            <div className="w-full min-w-3xs md:min-w-[600px] flex-1">
               <img src="/arrows.jpeg" alt="colorful arrows" className="w-full h-full object-contain object-top pt-3" />
             </div>
           </div>
@@ -119,7 +119,7 @@ export default function Page() {
               </div>
             </div>
             <div>
-              <img src="/book-cover.jpg" alt="Strategy Activation Playbook Cover" className="w-full h-full sm:w-max-sm object-contain rounded-2xl border border-gray-200" />
+              <img src="/book-cover.jpg" alt="Strategy Activation Playbook Cover" className="w-full h-full sm:max-w-sm object-contain rounded-2xl border border-gray-200" />
               <p className="mt-2 text-xs">Cover of The Strategy Activation Playbook</p>
             </div>
           </div>
@@ -135,7 +135,7 @@ export default function Page() {
               <p className="mt-3">With leadership roles at Intuit and Epinions.com, plus strategy work at Bain & Company, Aric brings deep experience in business strategy and innovation. He has also led and served on multiple nonprofit and for‑profit boards. He holds a BA from Johns Hopkins University and an MBA from Harvard Business School.</p>
             </div>
             <div className="lg:col-span-2">
-              <img src="/AW5.jpg" alt="Aric Wood headshot" className="w-full sm:w-max-sm rounded-2xl border border-gray-200 object-cover" />
+              <img src="/AW5.jpg" alt="Aric Wood headshot" className="w-full sm:max-w-sm object-contain rounded-2xl border border-gray-200" />
               <div className="flex gap-3 mt-3 text-sm">
                 <a target="_blank" href="#" className="btn btn-outline no-underline">Media Kit</a>
                 <a target="_blank" href="https://www.linkedin.com/in/aric-wood-588272/" className="btn btn-outline no-underline">LinkedIn</a>
